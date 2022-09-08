987d29175b136b13242129310723ee09793e9416
Abdulraheem <raimitaiwo45@gmail.com>
2022-09-08 17:34:25 +0100

    new commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let today = new Date()
+document.getElementById('days').innerHTML= today.getDate();
+document.getElementById('hours').innerHTML= today.getHours()
+document.getElementById('minutes').innerHTML= today.getMinutes()
+document.getElementById('seconds').innerHTML= today.getSeconds()
